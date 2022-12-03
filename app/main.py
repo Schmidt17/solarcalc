acd67968bff3cf92b8c17c98b3bc23c4d9cafc4e
ah <andreas.hofacker@posteo.de>
2022-12-03 20:29:12 +0100

    Add index.html template

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
-    return "<p>Welcome to solarcalc</p>"
+    return render_template('index.html')
