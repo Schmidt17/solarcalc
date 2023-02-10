5d3d6ab6bb6ad834ea118ddb76665a6e79c94811
ah <andreas.hofacker@posteo.de>
2023-02-10 21:22:31 +0100

    Implement calling calculation from endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import json
+import pandas as pd
+
+import solar_calculation.solar_benefits as sb
+from solar_calculation.solar_cell import SolarCell
 
 app = Flask(__name__)
 
@@ -9,4 +13,31 @@ def index():
         return render_template('index.html')
 
     elif request.method == 'POST':
-        return render_template('results.html', input_dict=request.form, results_dict={})
+        results_dict = process(request.form)
+
+        return render_template('results.html', input_dict=request.form, results_dict=results_dict)
+
+
+def process(input_dict):
+    longitude, latitude = json.loads(input_dict['coordinates'])
+
+    start_day = pd.Timestamp(input_dict['date-start'])
+    end_day = pd.Timestamp(input_dict['date-end'])
+
+    solar_cells = [
+        SolarCell(
+            float(input_dict['input-cell-1-inclination']),
+            float(input_dict['input-cell-1-azimuth']),
+            1e-3 * float(input_dict['input-cell-1-power'])
+        )
+    ]
+
+    results_dict = sb.calculate_solar_benefits(
+        latitude=latitude,
+        longitude=longitude,
+        start_day=start_day,
+        end_day=end_day,
+        solar_cells=solar_cells
+    )
+
+    return results_dict
